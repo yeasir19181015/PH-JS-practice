c8f4843e64ebc1d84774bbdb3928af60e5d57127
yeasir1918015 <yeasirarafat688@gmail.com>
2025-11-15 17:33:32 +0600

    delegate file added

---
+++ DOM/JS/bubble.js
@@ -0,0 +1,17 @@
+// list item 2 handler
+        document.getElementById("item-2")
+        .addEventListener('click', function(){
+            console.log("item 2 is clicked")
+        })
+
+        //ol click handler 
+        document.getElementById("item-list")
+        .addEventListener("click", function(){
+            console.log("ol clicked");
+            
+        })
+        //section click handler 
+        document.getElementById("item-list")
+        .addEventListener("click", function(){
+            console.log("section clicked");
+        })
