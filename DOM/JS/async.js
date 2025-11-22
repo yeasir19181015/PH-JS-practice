bd05b53077aadfdfc3751047ca9b27d5142511c8
yeasir1918015 <yeasirarafat688@gmail.com>
2025-11-22 16:48:15 +0600

    async added

---
+++ DOM/JS/async.js
@@ -0,0 +1,11 @@
+// const handleUser = () => {
+//     fetch ("https://jsonplaceholder.typicode.com/users")
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log("error dhorchi vai"))
+    
+// }
+
+
+const num1 = 10;
+const num2 = 20;
--- DOM/JS/post.js
+++ DOM/JS/post.js
@@ -1,3 +1,59 @@
 const handlePost = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => {
+        displayPost(data)
+    })
+}
 
+const displayPost = (posts) =>{
+    const postContainer = document.getElementById('postContainer');
+
+    for(const post of posts){
+       const div = document.createElement("div");
+       div.classList.add("card")
+       div.innerHTML = `
+       <h1>${post.title}</h1>
+       <p> ${post.body} </p>
+       <button>Details</button>
+       `
+       postContainer.appendChild(div)
+
+        
+    }
+    
+}
+
+
+const handleUpdate = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: 1,
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
+
+const handleCreate = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 }
