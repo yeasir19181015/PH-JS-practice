c6882fd79a846b3af3de47cd64b46519145988e5
yeasir1918015 <yeasirarafat688@gmail.com>
2025-11-27 23:56:33 +0600

    add setItem in local storage

---
+++ JS/storage.js
@@ -0,0 +1,10 @@
+const handleLocalStorage =() => {
+    const person ={
+        name :"yeasir",
+        age : 26,
+        friends : ["showon","noman"]
+    }
+    localStorage.setItem("yeasir",person)
+
+    
+}
